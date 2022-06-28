c42d4cbbd999689e26ab1b6cc53134c2f0d58968
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-28 15:53:19 -0300

    Iniciando o projeto Triva Game!

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
 
 ReactDOM.render(
   <BrowserRouter>
+  <Provider store={ store }> 
     <App />
+  </Provider>
   </BrowserRouter>,
 
   document.getElementById('root'),
