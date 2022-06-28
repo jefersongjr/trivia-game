744d8e615ccdd5a0ab78870d821896a21303c976
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-28 19:34:37 -0300

    criando as actions e o reducer de usuario

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,10 +1,16 @@
 const INITIAL_STATE = {
   userName: '',
   email: '',
+  token: {},
 };
 
 const loginReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+  case 'ADD_USER':
+    return ({ ...state,
+      userName: action.payload,
+      email: action.payload,
+      token: action.payload });
   default:
     return state;
   }
