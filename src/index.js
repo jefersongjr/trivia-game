c24a09672d2b925042b92b485943fdad9ddb1123
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-28 15:55:02 -0300

    criando provider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 ReactDOM.render(
