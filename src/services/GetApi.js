f15f3dabde5ebe7f9765cac8f46383d7156bc0dd
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-28 21:35:07 -0300

    requisito 2 , Salvando token no localStorage

--- src/services/GetApi.js
+++ src/services/GetApi.js
@@ -2,7 +2,8 @@ const getApiToken = async () => {
   const ENDPOINT = 'https://opentdb.com/api_token.php?command=request';
   const response = await fetch(`${ENDPOINT}`);
   const json = await response.json();
-  console.log(json);
+  localStorage.setItem('token', json.token);
+  console.log(json.token);
 
   return json;
 };
