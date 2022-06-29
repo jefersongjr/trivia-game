4dc77deb80173c464ae5ab96d7abe32eed79c6c6
Caio Pereira Antunes <caioefg@gmail.com>
2022-06-29 10:29:30 -0300

    continuação dos test req4

--- src/tests/01.Login.test.js
+++ src/tests/01.Login.test.js
@@ -1,39 +1,42 @@
 import React from 'react';
-// import userEvent from '@testing-library/user-event';
 import { screen } from '@testing-library/react';
 import renderWithRouterAndRedux  from './helpers/renderWithRouterAndRedux'
 import userEvent from '@testing-library/user-event';
 import App from '../App'
 
 
-const validEmail = 'a'
-const validName = 'b'
-
 describe('1- Crie a pagina de login', () => {
 
-    test('Verificar se a pagina contem o input name e email', () => {
+    test('Verificar se a pagina contém o input name, email, play e config / Se o botão esta inicialmente desativado e se ao clicar no play redireciona para a pagiina de jogar', () => {
         renderWithRouterAndRedux(<App />)
         const inputEmail = screen.getByTestId('input-gravatar-email')
         const inputName = screen.getByTestId('input-player-name')
+        const inputPlay = screen.getByRole('button', {  name: /play/i})
+        const inputConfig = screen.getByRole('button', {  name: /configurações/i})
         
+        expect(inputConfig).toBeInTheDocument()
         expect(inputEmail).toBeInTheDocument()
         expect(inputName).toBeInTheDocument()
+        expect(inputPlay).toBeInTheDocument()
+        expect(inputPlay).toBeDisabled()
+        userEvent.type(inputEmail, 'validEmail' )
+        userEvent.type(inputName, 'validName' )
+        expect(inputPlay).toBeEnabled();
+        userEvent.click(inputPlay)
+
+        const title = screen.getByTestId('header-player-name')
+        expect(title).toBeInTheDocument();
 
     })
-    test('Verifique se o button com text play esta na tela..', () => {
+
+    test('Verifique se ao clicar no botão config leva para tela de configuração', () => {
         renderWithRouterAndRedux(<App />)
-        const inputButton = screen.getByTestId('btn-play')
+        const inputConfig = screen.getByRole('button', {  name: /configurações/i})
+        expect(inputConfig).toBeInTheDocument()
 
-        expect(inputButton).toBeDisabled()
-        expect(inputButton).toBeInTheDocument()
-
-        const inputEmail = screen.getByTestId('input-gravatar-email')
-        const inputName = screen.getByTestId('input-player-name')
-
-        userEvent.type(inputEmail, validEmail )
-        userEvent.type(inputName, validName )
-        expect(inputButton).toBeEnabled();
-
-
+        userEvent.click(inputConfig)
+        const title = screen.getByTestId('settings-title')
+        expect(title).toBeInTheDocument()
     })
 })
+
