f545c5640552bcc80ea9adb923b5eb466c6fde00
jefersongjr <jeferson_gjr@hotmail.com>
2022-06-30 01:43:25 -0300

    requisito 6 , renderizand as perguntas

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,8 +4,14 @@ import { connect } from 'react-redux';
 import Header from '../component/Header';
 import { addQuestions } from '../redux/actions';
 import { getQuestions } from '../services/GetApi';
+import '../css/Game.css';
 
 class Game extends React.Component {
+  state = {
+    novoArray1: [],
+    // answer: [],
+  }
+
   async componentDidMount() {
     const token = localStorage.getItem('token');
     await getQuestions(token).then((resp) => {
@@ -18,26 +24,39 @@ class Game extends React.Component {
    shuffleAnswers = () => {
      const { questionResults } = this.props;
      // const novoArray = questionResults.map((objeto) => console.log(objeto.category));
-     const novoArray = questionResults.map((object) => ([
-       object.category,
-       object.question,
-       object.correct_answer,
-       object.incorrect_answers,
-     ]));
-
-     for (let i = novoArray.length - 1; i > 0; i -= 1) {
+     const novoArray = questionResults.map((object) => ({
+       category: object.category,
+       question: object.question,
+       answer: { correct: object.correct_answer, wrong: [...object.incorrect_answers] },
+     }));
+     /* for (let i = novoArray.length - 1; i > 0; i -= 1) {
        const j = Math.floor(Math.random() * (i + 1));
        [novoArray[i], novoArray[j]] = [novoArray[j], novoArray[i]];
-     }
+      } */
+
+     /* const answerArray = questionResults.map((object) => ([
+       object.correct_answer, ...object.incorrect_answers,
+     ])); */
+
+     this.setState({
+       novoArray1: [...novoArray],
+       // answer: [novoArray.answer.correct],
+     });
+
+     return novoArray;
    }
 
    render() {
-     const { questionResults } = this.props;
-     console.log(questionResults);
+     const { novoArray1 /* , answer */ } = this.state;
      return (
        <div>
          <Header />
-         {this.shuffleAnswers}
+         { novoArray1.map((x) => (
+           <div key={ x.category } className="container">
+             <p key={ x.category } className="container-text">{ x.category }</p>
+             <p key={ x.question } className="container-text">{ x.question }</p>
+           </div>
+         )) }
        </div>
      );
    }
