d1c9837039fd2bc3f505e387f96179abd4670f8b
Caio Pereira Antunes <caioefg@gmail.com>
2022-07-02 12:29:22 -0300

    req 13 14 15

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -5,13 +5,36 @@ import Header from '../component/Header';
 
 class Feedback extends React.Component {
   render() {
-    const { score } = this.props;
+    const { assertions, history, score } = this.props;
+    const n = 3;
     return (
       <div>
         <h1>FeedBack</h1>
         <Header />
-        <p>{ score }</p>
-        <p data-testid="feedback-text">Could be better...</p>
+        <p data-testid="feedback-total-question">{ assertions }</p>
+        <p data-testid="feedback-total-score">
+          { score }
+        </p>
+
+        <p>
+          {assertions >= n
+            ? <p data-testid="feedback-text">Well Done!</p>
+            : <p data-testid="feedback-text">Could be better...</p>}
+        </p>
+        <button
+          type="button"
+          onClick={ () => history.push('/') }
+          data-testid="btn-play-again"
+        >
+          Play Again
+        </button>
+        <button
+          type="button"
+          onClick={ () => history.push('/ranking') }
+          data-testid="btn-ranking"
+        >
+          Ranking
+        </button>
       </div>
     );
   }
@@ -19,10 +42,12 @@ class Feedback extends React.Component {
 
 const mapStateToProps = (state) => ({
   score: state.player.score,
+  assertions: state.player.assertions,
 });
 
 Feedback.propTypes = {
   score: PropTypes.number,
+  history: PropTypes.func,
 }.isRequired;
 
 export default connect(mapStateToProps)(Feedback);
