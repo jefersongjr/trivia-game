c37f07a932e396da618cde57d81916adb11a3188
Maraise <maraisegomes@gmail.com>
2022-07-02 13:28:11 -0300

    Item 16

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Feedback from './pages/Feedback';
 import Game from './pages/Game';
 import Login from './pages/Login';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/game" component={ Game } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
     </div>
   );
